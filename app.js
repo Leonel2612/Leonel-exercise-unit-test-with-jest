b98793cc77e4625122a1510b7234bd1382800ea6
Leonel2612 <77934438+Leonel2612@users.noreply.github.com>
2023-10-05 16:12:30 +0000

    Leonel-Exercise-unit-test

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ let oneEuroIs = {
 const fromDollarToYen=(usd)=>{
     let dollarToYen=(usd/oneEuroIs.USD)*oneEuroIs.JPY
     console.log(dollarToYen)
-    return dollarToYen.toFixed(2)
+    return dollarToYen
      }
 
 const fromEuroToDollar=(usd)=>{
     let euroToDollar=usd*oneEuroIs.USD
-    return euroToDollar.toFixed(2)
+    return euroToDollar
 }
 
 const fromYenToPound=(yen)=>{
     let yenToPound=yen*oneEuroIs.GBP
     console.log(yenToPound)
-    return yenToPound.toFixed(2)
+    return yenToPound
      }
 
 console.log(fromYenToPound(3.5))
--- test.js
+++ test.js
@@ -11,7 +11,7 @@ test("One euro should be 1.206 dollars", function(){
     const expected = 3.5 * 1.2; 
     
     // this is the comparison for the unit test
-     expect(fromEuroToDollar(3.5)).toBe("4.20"); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+     expect(dollars).toBe(expected); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
 })
 
 test("One euro should be 127.9 Yen", function(){
@@ -20,7 +20,7 @@ test("One euro should be 127.9 Yen", function(){
     const yen = fromDollarToYen(3.5)
     const expected = (3.5/1.2)*127.9 ; 
    
-     expect(fromDollarToYen(3.5)).toBe("373.04"); 
+     expect(yen).toBe(expected); 
 })
 
 
@@ -30,19 +30,6 @@ test("One euro should be 0.8", function(){
     const yen = fromYenToPound(3.5)
     const expected = 3.5*0.8 ; 
    
-     expect(fromYenToPound(3.5)).toBe("2.80"); 
+     expect(yen).toBe(expected); 
 })
 
-// test("One euro should be 1.206 dollars", function(){
-//     //import the function from app.js
-//     const { fromEuroToDollar } = require('./app.js')
-
-//     // use the function like its suppoed to be used
-//     const dollars = fromEuroToDollar(3.5)
-
-//     // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
-//     const expected = 3.5 * 1.2; 
-    
-//     // this is the comparison for the unit test
-//      expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
-// })
